2f8e4d3913577f96170938767bfe99b56286758f
Thaynan Andrey <thaynan.nunes@ccc.ufcg.edu.br>
2018-07-29 09:20:15 -0300

    Adicionada rota para cáculo de um fatorial

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
 app = Flask(__name__)
 
+# Rota para cálculo de fatorial passado por
+# parâmetro em /fatorial/<numero>.
+@app.route('/fatorial/<int:numero>')
+def calcularFatorial(numero):
+    return str(fatorial(numero))
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+## Cálcula o fatorial passado por parâmetro.
+def fatorial(numero):
+    if(numero < 0):
+        return -1
+    if(numero < 2):
+        return 1
+    return numero * fatorial(numero-1)
 
 if __name__ == '__main__':
     app.run()
