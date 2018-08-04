0fa7512137b743c44c6edaa7d6208f57ebc3843f
Gabriel Vinha <gabriel.vinha@ccc.ufcg.edu.br>
2018-08-04 00:18:37 -0300

    Add monitoring configuration

--- controller/controller.py
+++ controller/controller.py
@@ -1,17 +1,36 @@
 import time
+import ConfigParser
+import ast
+import logging
 
 from monitor.monitor import Monitor
 
 class Controller:
-    def __init__(self, host):
-        self.monitor = Monitor(host)
+    def __init__(self, clusters):
+        self.monitors = []
+        for cluster in clusters:
+            addr = config.get(cluster, "address")
+            logging.info("Updated configuration for cluster: " + cluster)
+            self.monitors.append(Monitor(cluster, addr))
 
     def run(self):
+        federated_metrics = {}
         while True:
-            status = self.monitor.check_status()
-            print status
+            for monitor in self.monitors:
+                status = monitor.check_status()
+                federated_metrics[status[0]] = status[1]
             time.sleep(5)
+            logging.info(federated_metrics)
+
+config = ConfigParser.ConfigParser()
+config.read('controller.cfg')
 
 if __name__ == "__main__":
-    controller = Controller("localhost:9090")
+    logging.basicConfig(filename='controller.log',level=logging.INFO)
+    logging.getLogger("requests").setLevel(logging.WARNING)
+    logging.info("Reading configuration file")
+    config = ConfigParser.ConfigParser()
+    config.read('controller.cfg')
+    clusters = config.sections()
+    controller = Controller(clusters)
     controller.run()
--- controller/monitor/monitor.py
+++ controller/monitor/monitor.py
@@ -1,9 +1,19 @@
+import requests
+import json
+
+import prometheus_client
+
 class Monitor:
-    def __init__(self, host):
-        self.endpoint = """
-            http://%s/api/v1/query?query=1%20-%20avg%20by(instance)(irate(node_cpu{mode=%27idle%27}[5m]))
-        """ % host
+    def __init__(self, name, host):
+        self.name = name
+        self.endpoint = "http://{0}/api/v1/query?query=avg%20by(instance){1}".format(
+            host,
+            "(irate(node_cpu{mode='idle'}[5m]))"
+        )
 
     def check_status(self):
-        pass
-
+        data = requests.get(self.endpoint).json()
+        metrics = []
+        for node in data['data']['result']:
+            metrics.append(1 - float(node['value'][1]))
+        return (self.name, metrics)
